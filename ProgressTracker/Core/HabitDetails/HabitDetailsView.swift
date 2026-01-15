c3ca4fe1433de11436312fa1220ea163a18b8931
kejlor <bartoszwojtkowiak.dev@gmail.com>
2026-01-15 17:21:56 +0100

    feat: update habit details

--- ProgressTracker/Core/HabitDetails/HabitDetailsView.swift
+++ ProgressTracker/Core/HabitDetails/HabitDetailsView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct HabitDetailsView: View {
     @State var presenter: HabitDetailsPresenter
-    let props: HabitProps
+    let habit: HabitModel
     
     var body: some View {
         Text("Habit Details View")
@@ -17,12 +17,12 @@ extension CoreRouter {
 
 }
 
-#Preview {
-    let container = DevPreview.shared.container
-    let builder = CoreBuilder(interactor: CoreInteractor(container: container))
-    let props = HabitProps()
-    
-    return RouterView { router in
-        builder.habitDetailsView(router: router, props: props)
-    }
-}
+//#Preview {
+//    let container = DevPreview.shared.container
+//    let builder = CoreBuilder(interactor: CoreInteractor(container: container))
+//    let props = HabitProps()
+//    
+//    return RouterView { router in
+//        builder.habitDetailsView(router: router, props: props)
+//    }
+//}

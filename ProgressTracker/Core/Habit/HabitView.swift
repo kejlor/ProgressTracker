0285e79085a520defb1196dbd7e8ae9f28698f1e
kejlor <bartoszwojtkowiak.dev@gmail.com>
2026-01-15 17:22:02 +0100

    feat: update habit view

--- ProgressTracker/Core/Habit/HabitView.swift
+++ ProgressTracker/Core/Habit/HabitView.swift
@@ -9,19 +9,20 @@ struct HabitProps: Identifiable {
 
 struct HabitView: View {
     @State var presenter: HabitPresenter
-    let props: HabitProps
+    let habit: HabitModel
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             HStack(alignment: .top) {
                 VStack(alignment: .leading) {
                     HStack {
-                        Text("\(props.days)")
+                        Text("\(habit.days)")
                             .font(.title)
                             .fontWeight(.bold)
                         
                         Image(systemName: "flame.fill")
-                            .foregroundStyle(props.selectedColor)
+//                            .foregroundStyle(habit.selectedColor)
+                            .foregroundStyle(Color(uiColor: hexStringToUIColor(hex: habit.selectedColor)))
                     }
                     
                     Text("DAYS")
@@ -35,13 +36,14 @@ struct HabitView: View {
                 } label: {
                     Image(systemName: "checkmark.circle.fill")
                         .resizable()
-                        .foregroundStyle(props.selectedColor)
+//                        .foregroundStyle(habit.selectedColor)
+                        .foregroundStyle(Color(uiColor: hexStringToUIColor(hex: habit.selectedColor)))
                         .frame(width: 25, height: 25)
                 }
             }
             .padding(.bottom, 20)
             
-            Text(props.name)
+            Text(habit.name)
                 .font(.footnote)
                 .fontWeight(.bold)
                 .padding(.bottom, 10)
@@ -55,7 +57,8 @@ struct HabitView: View {
         .padding(10)
         .background {
             RoundedRectangle(cornerRadius: 10)
-                .fill(props.selectedColor.opacity(0.2))
+//                .fill(habit.selectedColor.opacity(0.2))
+                .fill(Color(uiColor: hexStringToUIColor(hex: habit.selectedColor)).opacity(0.2))
         }
     }
 }
@@ -69,40 +72,63 @@ extension HabitView {
             Text(day)
                 .font(.caption)
             RoundedRectangle(cornerRadius: 4)
-                .fill(props.selectedColor)
+//                .fill(habit.selectedColor)
+                .fill(Color(uiColor: hexStringToUIColor(hex: habit.selectedColor)))
                 .frame(width: 15, height: 15)
         }
     }
 }
 
-#Preview("Single habit") {
-    let container = DevPreview.shared.container
-    let builder = CoreBuilder(interactor: CoreInteractor(container: container))
-    let props = HabitProps()
+func hexStringToUIColor(hex: String) -> UIColor {
+    var cString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
     
-    return RouterView { router in
-        builder.createHabitView(
-            router: router,
-            props: props
-        )
+    if cString.hasPrefix("#") {
+        cString.remove(at: cString.startIndex)
     }
+    
+    if cString.count != 6 {
+        return UIColor.gray
+    }
+    
+    var rgbValue:UInt64 = 0
+    Scanner(string: cString).scanHexInt64(&rgbValue)
+    
+    return UIColor(
+        red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+        green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+        blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+        alpha: CGFloat(1.0)
+    )
 }
 
-#Preview("Two habits") {
-    let container = DevPreview.shared.container
-    let builder = CoreBuilder(interactor: CoreInteractor(container: container))
-    let props = HabitProps()
-    
-    return RouterView { router in
-        HStack {
-            builder.createHabitView(
-                router: router,
-                props: props
-            )
-            builder.createHabitView(
-                router: router,
-                props: props
-            )
-        }
-    }
-}
+//#Preview("Single habit") {
+//    let container = DevPreview.shared.container
+//    let builder = CoreBuilder(interactor: CoreInteractor(container: container))
+//    let props = HabitProps()
+//    
+//    return RouterView { router in
+//        builder.createHabitView(
+//            router: router,
+//            props: props
+//        )
+//    }
+//}
+//
+//#Preview("Two habits") {
+//    let container = DevPreview.shared.container
+//    let builder = CoreBuilder(interactor: CoreInteractor(container: container))
+//    let props = HabitProps()
+//    
+//    return RouterView { router in
+//        HStack {
+//            builder.createHabitView(
+//                router: router,
+//                props: props
+//            )
+//            builder.createHabitView(
+//                router: router,
+//                props: props
+//            )
+//        }
+//    }
+//}

ddd2d5fa485e9fb6444818ff3bdc768257352fa8
kejlor <bartoszwojtkowiak.dev@gmail.com>
2026-01-19 16:23:15 +0100

    feat: add button type

--- ProgressTracker/Components/ViewModifiers/ButtonViewModifiers.swift
+++ ProgressTracker/Components/ViewModifiers/ButtonViewModifiers.swift
@@ -7,9 +7,14 @@
 
 import SwiftUI
 
+enum ButtonStyleOption {
+    case plain
+}
+
 extension View {
     @ViewBuilder
     func anyButton(
+        _ option: ButtonStyleOption = .plain,
         action: @escaping () -> Void
     ) -> some View {
         self.plainButton(action: action)
